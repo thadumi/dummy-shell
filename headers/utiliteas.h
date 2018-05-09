59f16906fd6d6f71f3f73540c7bfbc22f6fd978a
thadumi <dumitrescu_theodor_a_@hotmail.it>
2018-05-09 07:30:34 +0200

    fixed pipe between two commands, todo: fix the the pipe parser to call the new join function

---
+++ headers/utiliteas.h
@@ -0,0 +1,17 @@
+//
+// Created by thadumi on 5/6/18.
+//
+
+#ifndef SHELL_UTILITEAS_H
+#define SHELL_UTILITEAS_H
+
+#define FALSE 0
+#define TRUE !FALSE
+
+#define FALSE 0
+#define TRUE !FALSE
+
+typedef char bool;
+
+
+#endif //SHELL_UTILITEAS_H
