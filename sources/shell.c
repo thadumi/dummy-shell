4aa3a41517367adee186bd204d75d42ccaa617c0
thadumi <dumitrescu_theodor_a_@hotmail.it>
2018-05-10 10:40:12 +0200

    added a new TODO request to fix the shell's .config file

--- sources/shell.c
+++ sources/shell.c
@@ -20,10 +20,17 @@ int _nsh_exit(void) {
 extern char host_name[1024];
 extern char *current_dir;
 
-
+//TODO: gain the CLI arguments for shell config ... if they are unseated
+// check if there is a config file populated, otherwise create a default one
+// and ask the user if he want to create a new one or main the default settings
+// if he want to create a new one make a dialog to understand how he want to create it
+// another approach, only in linux is to execute the command "nano .config" so that
+// the user will be able to modify manually before the shell loop start.
 int nsh() {
 
+    //TODO: insert here the calls to config_function (after the stuffs that it is doing now the config function
     nsh_config();
+
     nsh_loop();
 
     return _nsh_exit();
