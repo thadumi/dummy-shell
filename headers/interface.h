a70a97dac606733810694f9858b76b178c8ac60c
Extrance <38425275+Extrance@users.noreply.github.com>
2018-05-26 17:39:42 +0200

    Add files via upload

---
+++ headers/interface.h
@@ -0,0 +1,13 @@
+//
+//  interface.h
+//  
+//
+//  Created by Andrea Balasso on 25/05/18.
+//
+
+#ifndef interface_h
+#define interface_h
+
+void personalizzazionefilelog ();
+
+#endif /* interface_h */
